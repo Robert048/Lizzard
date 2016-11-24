3b592b6988611daffbe249c845f641738e4d8a63
Robert048 <robertkarate@hotmail.com>
2016-11-24 15:35:44 +0100

    overwatch zooi

---
+++ Lizzard/Lizzard/Overwatch/Api.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lizzard
+{
+    class Api
+    {
+        public async Task<Data> get(string apiLink)
+        {
+            Data data = new Data(); ;
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var uri = new Uri(apiLink);
+                    var response = await client.GetAsync(uri);
+                    var result = await response.Content.ReadAsStringAsync();
+                    var jsonresult = JsonConvert.DeserializeObject<RootObject>(result);
+                    data = jsonresult.data;
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ Lizzard/Lizzard/Overwatch/OverwatchMainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Lizzard
+{
+    /// <summary>
+    /// Het overwatch mainscherm
+    /// </summary>
+    public sealed partial class OverwatchMainPage : Page
+    {
+        private string tag = "Bobgast-2232";
+        private string region = "eu";
+        private string platform = "pc";
+
+        public OverwatchMainPage()
+        {
+            getProfile();
+            this.InitializeComponent();
+        }
+
+        private async void getProfile()
+        {
+            Api call = new Api();
+            Data data = await call.get("https://api.lootbox.eu/" + platform + "/" + region + "/" + tag + "/profile");
+            image.Source = new BitmapImage(new Uri(data.avatar, UriKind.Absolute));
+            txtData.Text = data.username + System.Environment.NewLine + "Level: " + data.level;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
