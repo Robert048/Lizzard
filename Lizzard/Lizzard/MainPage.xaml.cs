329da4da46de5a5b4186e4a24270af43b05716ae
Robert048 <robertkarate@hotmail.com>
2016-12-02 19:28:20 +0100

    wow fixes

--- Lizzard/Lizzard/MainPage.xaml.cs
+++ Lizzard/Lizzard/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace Lizzard
 
         private void btnWOW_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(WorldOfWarcraftMainPage));
+            Frame.Navigate(typeof(World_of_Warcraft.MainPage));
         }
 
         private void btnD3_Click(object sender, RoutedEventArgs e)
