64830a21a1073f9075eb79a4e6aa28a9a7a157a7
Robert048 <robertkarate@hotmail.com>
2016-12-08 14:04:56 +0100

    dropdown region

--- Lizzard/Lizzard/LogInpage.xaml.cs
+++ Lizzard/Lizzard/LogInpage.xaml.cs
@@ -13,25 +13,39 @@ namespace Lizzard
         public LogInpage()
         {
             this.InitializeComponent();
+            boxRegion.Items.Add("eu");
+            boxRegion.Items.Add("us");
+            boxRegion.Items.Add("kr");
+            boxRegion.Items.Add("cn");
+            boxRegion.Items.Add("xbl");
+            boxRegion.Items.Add("psn");
             var tag = localSettings.Values["tag"];
             if (tag != null)
             {
                 User user = new User { tag = (string)localSettings.Values["tag"], region = (string)localSettings.Values["region"], platform = (string)localSettings.Values["platform"] };
                 txtUsername.Text = user.tag;
-                txtRegion.Text = user.region;
+                boxRegion.SelectedValue = user.region;
             }
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            if (txtUsername.Text != "" && txtRegion.Text != "")
+            if (txtUsername.Text != "" && boxRegion.SelectedIndex != -1)
             {
                 string username = txtUsername.Text;
                 if (username.Contains("#"))
                 {
                     username = username.Replace("#", "-");
                 }
-                User user = new User { tag = username, region = txtRegion.Text, platform = "pc" };
+                User user;
+                if(boxRegion.SelectedValue.ToString() == "psn" || boxRegion.SelectedValue.ToString() == "xbl")
+                {
+                    user = new User { tag = username, region = "global", platform = boxRegion.SelectedValue.ToString() };
+                }
+                else
+                {
+                    user = new User { tag = username, region = boxRegion.SelectedValue.ToString(), platform = "pc" };
+                }
                 localSettings.Values["tag"] = user.tag;
                 localSettings.Values["region"] = user.region;
                 localSettings.Values["platform"] = user.platform;
