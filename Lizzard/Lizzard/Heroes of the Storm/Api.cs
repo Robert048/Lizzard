6752fe7ed65c59e1f0f2e189ca381a7553d243a3
Robert048 <robertkarate@hotmail.com>
2016-12-12 11:47:27 +0100

    begin HotS

---
+++ Lizzard/Lizzard/Heroes of the Storm/Api.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lizzard.Heroes_of_the_Storm
+{
+    class Api
+    {
+    }
+}
