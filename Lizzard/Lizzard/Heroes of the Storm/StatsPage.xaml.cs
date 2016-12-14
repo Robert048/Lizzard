0a90f96f666fce7e0679f08563a2b5775bea7332
Robert048 <robertkarate@hotmail.com>
2016-12-14 11:55:29 +0100

    hots statspage tag+region fix

--- Lizzard/Lizzard/Heroes of the Storm/StatsPage.xaml.cs
+++ Lizzard/Lizzard/Heroes of the Storm/StatsPage.xaml.cs
@@ -2,25 +2,51 @@
 using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
+using Windows.UI.Xaml.Navigation;
 
 namespace Lizzard.Heroes_of_the_Storm
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Heroes of the Storm stat page
     /// </summary>
     public sealed partial class StatsPage : Page
     {
+        private User user;
+        Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
         public StatsPage()
         {
             this.InitializeComponent();
-            getStats();
         }
 
         private async void getStats()
         {
             Api call = new Api();
-            var result = await call.get("Players/2/Mandela_2595");
+            string region = "";
+            string tag = "";
+            switch (user.region)
+            {
+                case "us":
+                    region = "1";
+                    break;
+                case "eu":
+                    region = "2";
+                    break;
+                case "kr":
+                    region = "3";
+                    break;
+                case "cn":
+                    region = "4";
+                    break;
+                default:
+                    region = "1";
+                    break;
+            }
+            if (user.tag.Contains("-"))
+            {
+                tag = user.tag.Replace("-", "_");
+            }
+            var result = await call.get("Players/" + region + "/" + tag);
             var jsonresult = JsonConvert.DeserializeObject<RootObject>(result);
             txtStats.Text = txtStats.Text + jsonresult.Name + Environment.NewLine;
             foreach (var rank in jsonresult.LeaderboardRankings)
@@ -32,6 +58,21 @@ namespace Lizzard.Heroes_of_the_Storm
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var tag = localSettings.Values["tag"];
+            if (tag != null)
+            {
+                this.user = new User { tag = (string)localSettings.Values["tag"], region = (string)localSettings.Values["region"], platform = (string)localSettings.Values["platform"] };
+            }
+            else
+            {
+                Frame.Navigate(typeof(LogInpage));
+            }
+            getStats();
         }
     }
 }
