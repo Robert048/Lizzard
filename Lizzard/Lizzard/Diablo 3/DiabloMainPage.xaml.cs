ce99679aca29686657093585c09384d2ecd3523a
hakobk <hakobk1@gmail.com>
2017-01-09 13:03:54 +0100

    changes

--- Lizzard/Lizzard/Diablo 3/DiabloMainPage.xaml.cs
+++ Lizzard/Lizzard/Diablo 3/DiabloMainPage.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Lizzard.Diablo_3
 {
@@ -39,13 +26,13 @@ namespace Lizzard.Diablo_3
 
         private void btnProfile_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(DiabloProfile));
+            Frame.Navigate(typeof(Profile));
 
         }
 
         private void btnFollowers_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(DiabloFollowers));
+            Frame.Navigate(typeof(Followers));
         }
     }
 }
