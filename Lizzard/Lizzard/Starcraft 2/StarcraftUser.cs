f9b2c15be325d7c4adb1e836ee2558ff9d3f5bd4
hakobk <hakobk1@gmail.com>
2017-01-16 11:15:22 +0100

    starcraft2

---
+++ Lizzard/Lizzard/Starcraft 2/StarcraftUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lizzard.Starcraft_2
+{
+    class StarcraftUser
+    {
+        public string id;
+        public string name;
+    }
+}
